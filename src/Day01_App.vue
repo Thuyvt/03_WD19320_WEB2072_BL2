90e7f744165cf11319ea1e5f8b3be22e1b98418d
thuyvt <thuyvt1005@gmail.com>
2024-10-30 14:16:30 +0700

    first commit

---
+++ src/Day01_App.vue
@@ -0,0 +1,26 @@
+<!-- Khai báo biến, khai báo hàm -->
+<script setup>
+    // Khai báo thư viện
+    import { ref } from 'vue';
+
+    // Khai báo dạng chuỗi
+    const tenMonAn = "Trà sữa matcha full topping";
+    const danhMuc = ref("Trà sữa");
+    // Khai báo số
+    const soLuong = 2;
+    // Khai báo boolean
+    const trangThai = true;
+</script>
+
+<!-- Code HTML -->
+<template>
+  <div id="app">
+      <h3>Thông tin món ăn</h3>
+      <ul>
+        <li>Tên món: {{ tenMonAn }}</li>
+        <li>Danh mục: {{ danhMuc }}</li>
+        <li>Số lượng: {{ soLuong }}</li>
+        <li>Trạng thái: {{ trangThai }}</li>
+      </ul>
+  </div>
+</template>
